fdbb0aad03f92cc1814179308d530103e6e1bece
Abdullrahman <Abdullrahman.chaabouni123@gmail.com>
2024-05-07 23:52:58 +0300

    last

--- src/data_handlers/handler.ts
+++ src/data_handlers/handler.ts
@@ -1,11 +1,9 @@
 export interface TableHandler<T> {
-dropTable(): void;
-createTable(): void;
-    get(): T[];
-    getById(id: number): T;
-    insert(data: T): void;
-    update(data: T): void;
-    deleteById(id: number): void;
-
+  dropTable(): void;
+  createTable(): void;
+  get(): T[];
+  getById(id: number): T;
+  insert(data: T): void;
+  update(data: T): void;
+  deleteById(id: number): void;
 }
-
